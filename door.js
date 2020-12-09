7ac4ddc52e4e9dc84e4ca8adb8137cc91b61962b
Levan Velijanashvili <stichoza@gmail.com>
2020-12-10 01:41:29 +0400

    Simplify door actions

--- door.js
+++ door.js
@@ -3,14 +3,24 @@ const onoff = require('onoff');
 
 const { CurrentDoorState, TargetDoorState } = hap.Characteristic;
 
-const open = async (autoclose = 0) => {
+const press = async () => {
 
+};
+
+const open = async (autoclose = 0) => {
+    console.log('Green blink');
 };
 
 const close = async () => {
-
+    console.log('Red blink');
 };
 
 const status = async () => {
-    return CurrentDoorState.CLOSED;
+    console.log('Blue blink');
 };
+
+const identify = async () => {
+    console.log('Blue blink-blink-blink');
+};
+
+module.exports = { open, close, status, identify };
