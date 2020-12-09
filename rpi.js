936fd0972c897bb8fab930eeca88a1c34b87af30
Levan Velijanashvili <stichoza@gmail.com>
2020-12-09 04:31:54 +0400

    RPi device info

---
+++ rpi.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const serialFile = '/sys/firmware/devicetree/base/serial-number';
+const modelFile = '/sys/firmware/devicetree/base/model';
+
+const readFile = (file, caps = false, fallback = 'Unknown') => {
+    let content = fallback;
+
+    try {
+        content = fs.readFileSync(file);
+    } catch (e) {
+        return fallback;
+    }
+
+    return caps ? content.toUpperCase() : content;
+}
+
+module.exports = {
+    serial: () => {
+        return readFile(serialFile);
+    },
+    model: () => {
+        return readFile(modelFile, true);
+    }
+};
